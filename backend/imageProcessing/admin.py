c896eefd611ee15fa55785cbdf5dd6138643977a
Muthu Hariharan <164756456+muthuhariharan2001@users.noreply.github.com>
2025-06-04 16:38:01 +0530

    Delete backend directory

--- backend/imageProcessing/admin.py
+++
@@ -1,7 +0,0 @@
-# admin.py
-from django.contrib import admin
-from .models import ImageHashDBA, ImageHashDBB, HashPair
-
-admin.site.register(ImageHashDBA)
-admin.site.register(ImageHashDBB)
-admin.site.register(HashPair)
--- backend/imageProcessing/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class ImageHashSerializer(serializers.Serializer):
-    hash1 = serializers.CharField(required=False)
-    hash2 = serializers.CharField(required=False)
-    
